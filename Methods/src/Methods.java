f18f08998a0e42d09b4c15ff57a98443f06b634e
Miller Holliger <43080927+MillerHollinger@users.noreply.github.com>
2018-10-23 15:19:23 -0400

    Added new file

---
+++ Methods/src/Methods.java
@@ -0,0 +1,105 @@
+/* Project 5: Simple Methods
+ * Complete each of the puzzles, 1-20.
+ * Assure each function works correctly by running the program.
+ * Things of importance are marked with TODO.
+ * Good luck!
+ */
+
+// Layout of description: TODO DELETE WHEN DONE
+// Puzzle X
+/* TODO : <Description>
+ * USES: -/-
+ * ------PARAMETERS------
+ * <Type> <name> : <desc>
+ * 		<limits>
+ * ------RETURN VALUES------
+ * <IF, ELSE, RETURN>
+ */
+
+public class Methods {
+	public static void main(String[] args) {
+		// DO NOT EDIT THE MAIN. It tests your functions.
+	}
+
+	// Puzzle 1
+	/* TODO : Returns two raised to a power.
+	 * ------PARAMETERS------
+	 * int exponent : A power to raise 2 to.
+	 * 		exponent >= 0
+	 * ------RETURN VALUES------
+	 * RETURN 2 to the power of exponent.
+	 */
+	public static int powerOfTwo(int exponent) {
+
+		return -1; // Default return statement, delete.
+	}
+	// Puzzle 2
+	/* TODO : Returns the word "Alarm\n" repeated several times.
+	 * ------PARAMETERS------
+	 * int repeat : How many times to repeat "Alarm\n"
+	 * 		repeat > 0
+	 * ------RETURN VALUES------
+	 * RETURN "Alarm\n" repeated repeat times in a single String.
+	 */
+	public static String alarm(int repeat) {
+		
+		return ""; // Default return statement, delete.
+	}
+	// Puzzle 3
+	/* TODO : Sums and returns the numbers 1 to 100.
+	 * ------RETURN VALUES------
+	 * RETURN the sum of numbers 1 to 100, inclusive.
+	 */
+	public static int sum100() {
+		
+		return -1; // Default return statement, delete.
+	}
+	// Puzzle 4
+	/* TODO : Sums and returns the numbers in a range.
+	 * ------PARAMETERS------
+	 * int begin : The number to start adding from.
+	 * int end : The number to stop adding at (include it in the sum).
+	 * 		end > begin
+	 * ------RETURN VALUES------
+	 * RETURN the sum of numbers between begin and end.
+	 */
+	public static int sumRange(int begin, int end)	{
+		
+		return -1; // Default return statement, delete.
+	}
+	// Puzzle 5
+	/* TODO : Returns the larger of two integers.
+	 * ------PARAMETERS------
+	 * int a : An integer to compare.
+	 * int b : A second integer to compare.
+	 * ------RETURN VALUES------
+	 * RETURN the larger of a and b.
+	 */
+	public static int maxOfTwo(int a, int b) {
+		
+		return -1; // Default return statement, delete.
+	}
+	// Puzzle 6
+	/* TODO : Returns the larger of two doubles.
+	 * ------PARAMETERS------
+	 * double a : A double to compare.
+	 * double b : A second double to compare.
+	 * ------RETURN VALUES------
+	 * RETURN the larger of a and b.
+	 */
+	public static boolean larger(double a, double b){
+		
+		return false; // Default return statement, delete.
+	}
+	// Puzzle 7
+	/* TODO : Counts all the As in a String.
+	 * ------PARAMETERS------
+	 * String str : A String to process.
+	 * ------RETURN VALUES------
+	 * RETURN the amount of As in str.
+	 */
+	public static int countA(String str) {
+		
+		return -1; // Default return statement, delete.
+	}
+}
